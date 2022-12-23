8728fccdf549a78b5f158cae3d4c4fa1251713e3
PalakDevani <palak.devani85@gmail.com>
2022-12-23 21:42:11 +0000

    This is my change

--- src/main/java/com/nopcommerce/demo/pages/VidhiPage.java
+++ src/main/java/com/nopcommerce/demo/pages/VidhiPage.java
@@ -3,5 +3,8 @@ package com.nopcommerce.demo.pages;
 public class VidhiPage {
     public static void main(String[] args) {
         System.out.println("Helloooo");
+        System.out.println("new");
+
     }
+
 }
