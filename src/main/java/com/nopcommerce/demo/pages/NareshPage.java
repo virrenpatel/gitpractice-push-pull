ad6cc6011cd4d9476624cf2d50106933725d0637
Nareshgothadiya <naresh2u@gmail.com>
2022-12-29 22:59:20 +0000

    please accept

--- src/main/java/com/nopcommerce/demo/pages/NareshPage.java
+++ src/main/java/com/nopcommerce/demo/pages/NareshPage.java
@@ -4,5 +4,8 @@ public class NareshPage {
     public static void main(String[] args) {
         System.out.println("Hello Java team");
         System.out.println("Hope you enjoyed git session");
+        System.out.println("Hope you enjoyed git session");
+
+
     }
 }
